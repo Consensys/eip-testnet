2716e208e91b215b83bff18dbdadc5e7d2247a88
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-04-10 16:55:38 +0200

    fix

--- eip1559/geth/import-geth-account.sh
+++ eip1559/geth/import-geth-account.sh
@@ -4,8 +4,9 @@ source common/variables
 source common/functions
 
 NODE_ID=${1:-"1"}
+ACCOUNT_ID=${2:-"1"}
 DATA_DIR=$(gethDataDir $NODE_ID)
-KEY_FILE=$(gethKeyFile $NODE_ID)
+KEY_FILE=$(gethKeyFile $ACCOUNT_ID)
 PWD_FILE=$(gethPwdFile $NODE_ID)
 
 $GETH_PATH --datadir $DATA_DIR account import $KEY_FILE --password $PWD_FILE
--- eip1559/geth/run-geth.sh
+++ eip1559/geth/run-geth.sh
@@ -6,15 +6,18 @@ source common/functions
 NODE_ID=${1:-"1"}
 DATA_DIR=$(gethDataDir $NODE_ID)
 PWD_FILE=$(gethPwdFile $NODE_ID)
+NODE_KEY_FILE=$(gethNodeKeyFile $NODE_ID)
 RPC_PORT="$(($NODE_ID + 8544))"
 P2P_PORT="$(($NODE_ID + 30302))"
 
 BOOTNODE=$(cat $EIP1559_GETH_BOOTNODE_URL)
 $GETH_PATH --datadir $DATA_DIR \
+--nodiscover \
 --ipcdisable \
 --port $P2P_PORT --networkid $EIP1559_NETWORK_ID \
 --rpc --rpcport $RPC_PORT --rpcaddr 0.0.0.0 \
 --rpcapi "admin,web3,eth,net,personal,txpool" --rpccorsdomain "0.0.0.0" \
---txpool.nolocals --rpcvhosts=*  --nousb --etherbase $COINBASE \
+--rpcvhosts=*  --nousb --etherbase $COINBASE \
 --unlock 0 --allow-insecure-unlock \
---password $PWD_FILE
+--password $PWD_FILE \
+--nodekey $NODE_KEY_FILE
