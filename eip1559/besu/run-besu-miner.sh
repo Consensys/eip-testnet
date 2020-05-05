21161521da59142cfeb5140f7424f7a2c1993b18
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-05-05 11:33:43 +0200

    fix

--- eip1559/besu/run-besu-miner.sh
+++ eip1559/besu/run-besu-miner.sh
@@ -11,6 +11,13 @@ $BESU_PATH --genesis-file=$EIP1559_BESU_GENESIS \
 --data-path=$DATA_DIR --rpc-http-cors-origins=all --discovery-enabled=false \
 --rpc-http-enabled --rpc-http-apis=ETH,NET,WEB3,DEBUG,ADMIN,TRACE,PLUGINS  \
 --p2p-enabled=true --p2p-port=30305 --rpc-http-port=8547 \
---Xeip1559-enabled=true \
 --miner-enabled --miner-coinbase=0xf17f52151EbEF6C7334FAD080c5704D77216b732 \
---logging=info
+--logging=info \
+--Xeip1559-enabled=true \
+--Xeip1559-basefee-max-change-denominator=8 \
+--Xeip1559-target-gas-used=10000000 \
+--Xeip1559-slack-coefficient=2 \
+--Xeip1559-decay-range=800000 \
+--Xeip1559-gas-increment-amount=10 \
+--Xeip1559-initial-base-fee=1000000000 \
+--Xeip1559-per-tx-gas-limit=8000000 \
