ae6b8f848fd9a8ea668278fee753736c22c37f69
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-06-25 10:51:31 +0200

    Update run-besu-miner.sh

--- eip1559/besu/run-besu-miner.sh
+++ eip1559/besu/run-besu-miner.sh
@@ -6,13 +6,12 @@ source common/functions
 NODE_ID=${1:-"1"}
 DATA_DIR=$(besuDataDir $NODE_ID)
 
-echo $DATA_DIR
 $BESU_PATH --genesis-file=$EIP1559_BESU_GENESIS \
 --data-path=$DATA_DIR --rpc-http-cors-origins=all --discovery-enabled=false \
 --rpc-http-enabled --rpc-http-apis=ETH,NET,WEB3,DEBUG,ADMIN,TRACE,PLUGINS  \
 --p2p-enabled=true --p2p-port=30305 --rpc-http-port=8547 \
 --miner-enabled --miner-coinbase=0xf17f52151EbEF6C7334FAD080c5704D77216b732 \
---logging=off \
+--logging=INFO \
 --Xeip1559-enabled=true \
 --Xeip1559-basefee-max-change-denominator=8 \
 --Xeip1559-target-gas-used=10000000 \
